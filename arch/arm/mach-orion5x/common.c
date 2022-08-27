a3b78f65a10335359f1abf8d82c6d9df49691737
Pali Rohár <pali@kernel.org>
2022-08-27 02:08:57 +0200

    ARM: orion: Move PCIe mbus window mapping from orion5x_setup_wins() to pcie_setup()
    This would allow to migrate Orion PCIe code to pci-mvebu.c driver as this
    driver reads mapping information from device tree files and does not relay
    on static mappings.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>

--- arch/arm/mach-orion5x/common.c
+++ arch/arm/mach-orion5x/common.c
@@ -228,19 +228,6 @@ void __init orion5x_init_early(void)
 
 void orion5x_setup_wins(void)
 {
-	/*
-	 * The PCIe windows will no longer be statically allocated
-	 * here once Orion5x is migrated to the pci-mvebu driver.
-	 */
-	mvebu_mbus_add_window_remap_by_id(ORION_MBUS_PCIE_IO_TARGET,
-					  ORION_MBUS_PCIE_IO_ATTR,
-					  ORION5X_PCIE_IO_PHYS_BASE,
-					  ORION5X_PCIE_IO_SIZE,
-					  ORION5X_PCIE_IO_BUS_BASE);
-	mvebu_mbus_add_window_by_id(ORION_MBUS_PCIE_MEM_TARGET,
-				    ORION_MBUS_PCIE_MEM_ATTR,
-				    ORION5X_PCIE_MEM_PHYS_BASE,
-				    ORION5X_PCIE_MEM_SIZE);
 	mvebu_mbus_add_window_remap_by_id(ORION_MBUS_PCI_IO_TARGET,
 					  ORION_MBUS_PCI_IO_ATTR,
 					  ORION5X_PCI_IO_PHYS_BASE,
